1d98a041e8b2ff9a7eccb6f9ee7d96db07f8c3ca
M.Atif63 <muhammed.atif.ali.63@gmail.com>
2026-02-24 07:57:11 +0500

    Difference between function decleration and function definition

---
+++ app.js
@@ -0,0 +1,47 @@
+// console.log("hello")
+
+// Function Decleration
+// appKaName("ali")
+// function appKaName(name){
+//     console.log(`mera Name ${name} hai`)
+// }
+
+
+
+// Function Expression or Definition
+// var appKaName = function(name){
+//     console.log(`mera name ${name} hai`)
+// }
+// appKaName("ali")
+
+
+
+// Arrow Function
+// var appKaName = (x,y) => {
+//     console.log(`total ${x+y}`)
+// }
+// appKaName(2,3)
+
+
+// Function Constructor
+// function Person(naam, umer, degree){
+//     this.name = naam;
+//     this.age = umer;
+//     this.taleem = degree;
+
+//     console.log(`mera naam ${this.name} hai`)
+// }
+
+// var per1 = new Person("ali",18,"BBA")
+// var per2 = new Person("ahmed",20,"MBA")
+// console.log(per1.name)
+
+
+// Object Method
+// var objectMethod = {
+//     myfunc : function (x,y) {
+//         console.log(x+y)
+//     }
+// }
+// objectMethod.myfunc(10,100)
+
